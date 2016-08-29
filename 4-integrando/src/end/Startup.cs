060b8a9c9df33ae0f3cc8993c2b4b2d483957aa9
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-08-29 00:44:03 -0300

    Updated end solution for module 4

--- 4-integrando/src/end/Startup.cs
+++ 4-integrando/src/end/Startup.cs
@@ -33,7 +33,17 @@ namespace TourOfHeroes
 
       app.UseStaticFiles();
       
-      app.UseMvc();
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+          name: "default",
+          template: "{controller=Home}/{action=Index}/{id?}");
+
+        routes.MapRoute(
+          name: "spa-fallback",
+          template: "{*url}",
+          defaults: new { controller = "Home", action = "Index" });
+      });
     }
   }
 }
