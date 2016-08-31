46547d1ca71357e2188aa8aca89e22d47af908b7
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-08-30 23:10:33 -0300

    Added begin and end solution (WIP)

---
+++ 5-javascript-services/src/begin/Models/Hero.cs
@@ -0,0 +1,9 @@
+namespace TourOfHeroes.Models
+{
+  public class Hero
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+  }
+}
---
+++ 5-javascript-services/src/begin/config/webpack.dev-aspnet.js
@@ -0,0 +1,18 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helpers');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+
+  output: {
+      path: helpers.root('wwwroot'),
+      filename: '[name].js',
+      publicPath: '/'
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ]
+});
