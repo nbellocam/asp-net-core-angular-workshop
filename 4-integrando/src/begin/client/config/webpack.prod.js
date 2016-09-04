f8cfb4adf94a613dd04f8a5496ccfc261317638e
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-09-04 12:12:12 -0300

    Fixed issues with begin solution for module 4

--- 4-integrando/src/begin/client/config/webpack.prod.js
+++ 4-integrando/src/begin/client/config/webpack.prod.js
@@ -10,9 +10,9 @@ module.exports = webpackMerge(commonConfig, {
   devtool: 'source-map',
 
   output: {
-      path: helpers.root('wwwroot', 'dist'),
+      path: helpers.root('dist'),
       filename: '[name].js',
-      publicPath: '/dist/'
+      publicPath: '/'
   },
 
   htmlLoader: {
