da22d47f167da08df31bcbd288b61ef3a6907449
Nicolas Bello Camilletti <nbellocamilletti@gmail.com>
2016-09-05 01:54:13 -0300

    Updated end solution for module 5

--- 5-javascript-services/src/end/Startup.cs
+++ 5-javascript-services/src/end/Startup.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 
 namespace TourOfHeroes
 {
